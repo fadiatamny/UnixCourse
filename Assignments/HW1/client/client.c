9552b6b9bcea97335c03a837c765316cd6808335
Fadi AT <fadiatamny@gmail.com>
2019-08-14 16:35:26 +0300

    fixes in the first exercies

--- Assignments/HW1/client/client.c
+++ Assignments/HW1/client/client.c
@@ -1,4 +1,17 @@
-#include "../includes.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define PORT 0x0da2
 #define IP_ADDR 0x7f000001
